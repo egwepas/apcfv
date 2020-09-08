0878b6618ebe3d0010c216dd277bdf1c4098ef86
egwepas <gwenhael.pasquiers@ericsson.com>
2020-09-08 08:35:19 +0200

    add support for gzipped files

--- src/main/java/com/github/egwepas/apcfv/Parser.java
+++ src/main/java/com/github/egwepas/apcfv/Parser.java
@@ -7,8 +7,10 @@ package com.github.egwepas.apcfv;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,6 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 
 /**
  *
@@ -29,55 +32,63 @@ public class Parser {
     public Map<String, ProfilerMethods> methodsMap = new HashMap<>();
 
     public Parser(File f) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(f))) {
-            String line;
-            while (null != (line = br.readLine())) {
-                int stackStart;
-                String thread;
-                if (line.startsWith("[")) {
-                    stackStart = line.indexOf(']') + 2;
-                    thread = line.substring(1, stackStart - 2);
-                } else {
-                    thread = "default";
-                    stackStart = 0;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(f))))){
+            parse(br);
+        } catch(Exception e){
+            try(BufferedReader br = new BufferedReader(new FileReader(f))){
+                parse(br);
+            }
+        }
+    }
+
+    public void parse(BufferedReader br) throws IOException {
+        String line;
+        while (null != (line = br.readLine())) {
+            int stackStart;
+            String thread;
+            if (line.startsWith("[")) {
+                stackStart = line.indexOf(']') + 2;
+                thread = line.substring(1, stackStart - 2);
+            } else {
+                thread = "default";
+                stackStart = 0;
+            }
+            try {
+                int countIndex = line.lastIndexOf(' ');
+                int count = Integer.valueOf(line.substring(countIndex + 1));
+                String stack = line.substring(stackStart, countIndex);
+                List<String> methods = Arrays.asList(stack.split(Pattern.quote(";")));
+
+                if (!forwardTrees.containsKey(thread)) {
+                    forwardTrees.put(thread, new ProfilerTree());
                 }
-                try {
-                    int countIndex = line.lastIndexOf(' ');
-                    int count = Integer.valueOf(line.substring(countIndex + 1));
-                    String stack = line.substring(stackStart, countIndex);
-                    List<String> methods = Arrays.asList(stack.split(Pattern.quote(";")));
+                forwardTrees.get(thread).incCount(count);
+                forwardTrees.get(thread).add(methods.iterator(), count);
 
-                    if (!forwardTrees.containsKey(thread)) {
-                        forwardTrees.put(thread, new ProfilerTree());
-                    }
-                    forwardTrees.get(thread).incCount(count);
-                    forwardTrees.get(thread).add(methods.iterator(), count);
+                Collections.reverse(methods);
 
-                    Collections.reverse(methods);
-
-                    if (!backwardTrees.containsKey(thread)) {
-                        backwardTrees.put(thread, new ProfilerTree());
-                    }
-                    backwardTrees.get(thread).incCount(count);
-                    backwardTrees.get(thread).add(methods.iterator(), count);
-
-                    if (!backwardMethods.containsKey(thread)) {
-                        backwardMethods.put(thread, new ProfilerTree());
-                    }
-                    backwardMethods.get(thread).incCount(count);
-                    for(int i=0; i<methods.size() - 1; i++){
-                        backwardMethods.get(thread).add(methods.subList(i, methods.size() - 1).iterator(), count);
-                    }
-
-                    if (!methodsMap.containsKey(thread)) {
-                        methodsMap.put(thread, new ProfilerMethods());
-                    }
-
-                    methodsMap.get(thread).add(methods, count);
-                } catch (Exception e) {
-                    System.err.println("Error handling line : " + line);
-                    e.printStackTrace();
+                if (!backwardTrees.containsKey(thread)) {
+                    backwardTrees.put(thread, new ProfilerTree());
                 }
+                backwardTrees.get(thread).incCount(count);
+                backwardTrees.get(thread).add(methods.iterator(), count);
+
+                if (!backwardMethods.containsKey(thread)) {
+                    backwardMethods.put(thread, new ProfilerTree());
+                }
+                backwardMethods.get(thread).incCount(count);
+                for (int i = 0; i < methods.size() - 1; i++) {
+                    backwardMethods.get(thread).add(methods.subList(i, methods.size() - 1).iterator(), count);
+                }
+
+                if (!methodsMap.containsKey(thread)) {
+                    methodsMap.put(thread, new ProfilerMethods());
+                }
+
+                methodsMap.get(thread).add(methods, count);
+            } catch (Exception e) {
+                System.err.println("Error handling line : " + line);
+                e.printStackTrace();
             }
         }
     }
